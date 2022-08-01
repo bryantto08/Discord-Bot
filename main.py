829212ab98fafe0be39c176425aaceeb2d500b2c
bryantto08 <bryantto08@gmail.com>
2022-08-01 13:02:44 -0400

    updated error functions

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ riot_key = os.getenv("riot_key")
 lol_watcher = LolWatcher(riot_key)
 
 
-class PointGameBot(commands.Bot):
+class StarBot(commands.Bot):
     # Initializes the bot, determines its prefix as $
     def __init__(self):
         super().__init__(
@@ -45,20 +45,28 @@ class PointGameBot(commands.Bot):
                 await channel.send("No argument found")
 
         @self.command(brief="sends back league level")
-        async def summoner(channel, arg):
-            player_info = lol_watcher.summoner.by_name("na1", arg)
-            await channel.send(player_info["summonerLevel"])
+        async def summoner(channel, summoner_name):
+            player_info = lol_watcher.summoner.by_name("na1", summoner_name)
+            await channel.send("Level: " + str(player_info["summonerLevel"]))
+
+        @summoner.error
+        async def summoner_error(channel, error):
+            await channel.send("Summoner not found!")
 
         @self.command(brief="sends back champ mastery info")
-        async def champ_mastery(channel, arg):
+        async def mastery(channel, summoner_name):
             # Helper functions located in helpers.py
-            player_info = lol_watcher.summoner.by_name("na1", arg)
+            player_info = lol_watcher.summoner.by_name("na1", summoner_name)
             player_mastery = lol_watcher.champion_mastery.by_summoner("na1", player_info["id"])
             my_string = "Top 5 Champions: " + "\n"
             for i in range(5):
                 my_string += champ_mastery_string(player_mastery[i]) + "\n"
             await channel.send("```" + my_string + "```")
 
+        @mastery.error
+        async def mastery_error(channel, error):
+            await channel.send("Summoner not found!")
 
-bot = PointGameBot()
+
+bot = StarBot()
 bot.run(discord_key)
