86f794e2aff3f17b3a96cbc7ef34e4213bc9dcc0
bryantto08 <bryantto08@gmail.com>
2022-08-01 12:45:03 -0400

    champ mastery functionality

---
+++ helpers.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import json
+
+
+def change_to_timestamp(time):
+    timestamp = datetime.fromtimestamp(time)
+    return timestamp
+
+
+# finds the champion that corresponds to the key in the data_dragon.json file
+def find_champ(key):
+    json_file = open("data_dragon.json", "r", encoding="utf-8")
+    data_dragon = json.load(json_file)
+    for i in data_dragon["data"]:
+        if data_dragon["data"][i]["key"] == str(key):
+            return i
+
+
+# Turns champ_mastery dictionary into readable and easy to add string
+def champ_mastery_string(player_dict):
+    champ = find_champ(player_dict["championId"])
+    champ_level = player_dict["championLevel"]
+    champ_points = player_dict["championPoints"]
+    last_played = change_to_timestamp((player_dict["lastPlayTime"])/1000.0)
+    champ_points_til_next_level = player_dict["championPointsUntilNextLevel"]
+    chest_granted = player_dict["chestGranted"]
+    champ_string = "Champion: " + champ + " | " + "Champion Level: " + str(champ_level) + " | " \
+                   "Champion Points: " + str(champ_points) + " | " + "Last Played: " + str(last_played)
+    return champ_string
+
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import discord
 from discord.ext import commands
 import os
+import subprocess
 from dotenv import load_dotenv
+from riotwatcher import LolWatcher, ApiError
+from datetime import datetime
 import asyncio
+
+import helpers
+from helpers import *
 
 client = discord.Client()
 load_dotenv()
 discord_key = os.getenv("discord_key")
+riot_key = os.getenv("riot_key")
+lol_watcher = LolWatcher(riot_key)
 
 
 class PointGameBot(commands.Bot):
@@ -26,15 +34,30 @@ class PointGameBot(commands.Bot):
             print("Bot logged in!")
 
         @self.command(brief="sends back text")
-        async def test(ctx, arg):
-            await ctx.send(arg)
+        async def test(channel, arg):
+            await channel.send(arg)
 
         # Function occurs when test function has an error
         @test.error
-        async def test_error(ctx, error):
+        async def test_error(channel, error):
             # isinstance function checks if specified object(error) is of specified type (error type)
             if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-                await ctx.send("No argument found")
+                await channel.send("No argument found")
+
+        @self.command(brief="sends back league level")
+        async def summoner(channel, arg):
+            player_info = lol_watcher.summoner.by_name("na1", arg)
+            await channel.send(player_info["summonerLevel"])
+
+        @self.command(brief="sends back champ mastery info")
+        async def champ_mastery(channel, arg):
+            # Helper functions located in helpers.py
+            player_info = lol_watcher.summoner.by_name("na1", arg)
+            player_mastery = lol_watcher.champion_mastery.by_summoner("na1", player_info["id"])
+            my_string = "Top 5 Champions: " + "\n"
+            for i in range(5):
+                my_string += champ_mastery_string(player_mastery[i]) + "\n"
+            await channel.send("```" + my_string + "```")
 
 
 bot = PointGameBot()
