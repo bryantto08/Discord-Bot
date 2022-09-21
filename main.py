defdb671a042d330aacdf69bfba80f810aa58ce2
Bryant <>
2022-09-21 22:03:08 +0000

    continuous uptime on the bot

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from riotwatcher import LolWatcher, ApiError, TftWatcher
 from datetime import datetime
 import asyncio
 # import PyNaCl
+from keep_alive import keep_alive
 
 import helpers
 from helpers import *
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 load_dotenv()
 discord_key = os.getenv("discord_key")
 riot_key = os.getenv("riot_key")
@@ -22,13 +24,11 @@ tft_watcher = TftWatcher(riot_key)
 class StarBot(commands.Bot):
     # Initializes the bot, determines its prefix as $
     def __init__(self):
-        super().__init__(
-            command_prefix="*",
-            help_command=commands.DefaultHelpCommand(no_category="List of Commands")
-        )
-        self.bot = commands.Bot(
-            command_prefix="*"
-        )
+        super().__init__(command_prefix="*",
+                         intents=intents,
+                         help_command=commands.DefaultHelpCommand(
+                             no_category="List of Commands"))
+        self.bot = commands.Bot(command_prefix="*", intents=intents)
         self.count = 0
 
         # When the bot is officially online
@@ -47,15 +47,18 @@ class StarBot(commands.Bot):
         @test.error
         async def test_error(channel, error):
             # isinstance function checks if specified object(error) is of specified type (error type)
-            if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+            if isinstance(error,
+                          discord.ext.commands.errors.MissingRequiredArgument):
                 await channel.send("No argument found")
 
         @self.command(brief="sends back league info")
         async def summoner(channel, summoner_name):
             player_info = lol_watcher.summoner.by_name("na1", summoner_name)
             player_level = player_info["summonerLevel"]
-            player_rank = lol_watcher.league.by_summoner("na1", player_info["id"])
-            player_tft_rank = tft_watcher.league.by_summoner("na1", player_info["id"])
+            player_rank = lol_watcher.league.by_summoner(
+                "na1", player_info["id"])
+            player_tft_rank = tft_watcher.league.by_summoner(
+                "na1", player_info["id"])
             my_string = f"{summoner_name} (Level {player_level}): " + "\n"
             for i in range(len(player_rank)):
                 my_string += summoner_string(player_rank[i]) + "\n"
@@ -75,7 +78,8 @@ class StarBot(commands.Bot):
         async def mastery(channel, *, summoner_name):
             # Helper functions located in helpers.py
             player_info = lol_watcher.summoner.by_name("na1", summoner_name)
-            player_mastery = lol_watcher.champion_mastery.by_summoner("na1", player_info["id"])
+            player_mastery = lol_watcher.champion_mastery.by_summoner(
+                "na1", player_info["id"])
             my_string = "Top 5 Champions: " + "\n"
             for i in range(5):
                 my_string += champ_mastery_string(player_mastery[i]) + "\n"
@@ -93,11 +97,13 @@ class StarBot(commands.Bot):
             # Used to retrieve summoner id
             player_info = lol_watcher.summoner.by_name("na1", summoner_name)
             # Returns a json dictionary containing 5 matches
-            player_match_history = lol_watcher.match.matchlist_by_puuid("na1", player_info["puuid"], count=5)
+            player_match_history = lol_watcher.match.matchlist_by_puuid(
+                "na1", player_info["puuid"], count=5)
             my_string = ""
             for i in player_match_history:
                 match = lol_watcher.match.by_id("na1", i)
-                my_string += match_history_string(match, summoner_name) + "\n" + "\n"
+                my_string += match_history_string(match,
+                                                  summoner_name) + "\n" + "\n"
             self.count += 1
             if sus_check():
                 await channel.send("sus")
@@ -114,6 +120,6 @@ class StarBot(commands.Bot):
         #         await channel.voice_client.move_to(voice_channel)
 
 
-
 bot = StarBot()
+keep_alive()
 bot.run(discord_key)
